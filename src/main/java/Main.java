9d030ea2fccbc542dcf59952be1dbaae546ae2cb
tritekstyn <tritekstyn@gmail.com>
2020-11-26 17:19:31 +0100

    lol

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("B-)");
+        int x = 1;
+        int y = 3;
+        System.out.println(x + y);
     }
 }
